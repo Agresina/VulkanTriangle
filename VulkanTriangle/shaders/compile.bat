2f3c5d789c364d806d08246b0ec955d33eb47266
Agresina <agresina@gmail.com>
2021-03-13 12:06:11 +0100

    Subida inicial
    Subida inicial con el código en tras haber realizado la sección "Drawing a triangle" del tutorial de Vulkan (https://vulkan-tutorial.com/en/Drawing_a_triangle/)

---
+++ VulkanTriangle/shaders/compile.bat
@@ -0,0 +1,4 @@
+set vulkanGccPath="C:\Desarrollo\Programas\VulkanSDK\1.2.154.1\Bin\glslc.exe"
+C:\Desarrollo\Programas\VulkanSDK\1.2.154.1\Bin\glslc.exe shader.vert -o vert.spv
+C:\Desarrollo\Programas\VulkanSDK\1.2.154.1\Bin\glslc.exe shader.frag -o frag.spv
+pause
